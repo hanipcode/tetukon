f41228060c5a39d2845b664d8c36a3648d34f348
Muhammad Hanif <hanifeij@yahoo.co.id>
2025-05-29 22:12:22 +0700

    feat: fixes alb/main.tf

--- infrastructure/terraform/modules/alb/main.tf
+++ infrastructure/terraform/modules/alb/main.tf
@@ -115,8 +115,13 @@ resource "aws_lb_listener" "https" {
   certificate_arn   = var.ssl_certificate_arn
 
   default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.traefik.arn
+    type = "forward"
+    
+    forward {
+      target_group {
+        arn = aws_lb_target_group.traefik.arn
+      }
+    }
   }
 }
 
